79fc65bccc3bb572a431f2b3faf504cdff240847
Denitsa Pambelova <deni.pambelova@gmail.com>
2021-04-09 18:13:23 +0300

    setting login and logout

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -38,6 +38,24 @@ const userCtrl = {
             return res.status(500).json({msg: err.message})
         }
     },
+    login: async (req,res)=>{
+        try{
+            const {email,password}=req.body;
+
+            const user= await Users.findOne({email})
+            if(!user) return res.status(400).json({msg: "User does not exist"})
+
+            const isMatch= await bcrypt.compare(password,user.password)
+            if(!isMatch) return res.status(400).json({msg: "Incorrect password"})
+
+            // If login success, create access token and refresh token
+
+            res.json({msg: "Login success!"})
+
+        }catch(err){
+            return res.status(500).json({msg: err.message})
+        }
+    },
     refreshToken: (req, res) =>{
         try {
             const rf_token = req.cookies.refreshtoken;
